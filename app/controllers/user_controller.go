8c99cde663e7b562d83c3c069b94974efd1e95e8
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-11-12 18:33:25 +0700

    feat(user-management): Make Login API and connect in to DB & Make JWT and Refresh Token

--- app/controllers/user_controller.go
+++ app/controllers/user_controller.go
@@ -5,8 +5,10 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/kooroshh/fiber-boostrap/app/models"
 	"github.com/kooroshh/fiber-boostrap/app/repository"
+	"github.com/kooroshh/fiber-boostrap/pkg/jwt_token"
 	"github.com/kooroshh/fiber-boostrap/pkg/response"
 	"golang.org/x/crypto/bcrypt"
+	"time"
 )
 
 func Register(ctx *fiber.Ctx) error {
@@ -45,4 +47,62 @@ func Register(ctx *fiber.Ctx) error {
 	resp.Password = ""
 
 	return ctx.SendStatus(fiber.StatusOK)
+}
+
+func Login(ctx *fiber.Ctx) error {
+
+	var (
+		loginReq = new(models.LoginRequest)
+		resp     = models.LoginResponse{}
+		now      = time.Now()
+	)
+
+	err := ctx.BodyParser(loginReq)
+	if err != nil {
+		errResponse := fmt.Errorf("failed to parse body request: %v", err)
+		fmt.Println("Failed to parse body request: ", err)
+		return response.SendFailureResponse(ctx, fiber.StatusInternalServerError, errResponse.Error(), nil)
+	}
+
+	err = loginReq.Validate()
+	if err != nil {
+		errResponse := fmt.Errorf("failed to validate body request: %v", err)
+		fmt.Println("Failed to validate body request: ", err)
+		return response.SendFailureResponse(ctx, fiber.StatusInternalServerError, errResponse.Error(), nil)
+	}
+
+	user, err := repository.GetUserByUsername(ctx.Context(), loginReq.Username)
+	if err != nil {
+		errResponse := fmt.Errorf("failed to get user by username: %v", err)
+		fmt.Println(errResponse)
+		return response.SendFailureResponse(ctx, fiber.StatusNotFound, "username/password is wrong", nil)
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginReq.Password))
+	if err != nil {
+		errResponse := fmt.Errorf("failed to compare hash password: %v", err)
+		fmt.Println(errResponse)
+		return response.SendFailureResponse(ctx, fiber.StatusNotFound, "username/password is wrong", nil)
+	}
+
+	token, err := jwt_token.GenerateToken(ctx.Context(), user.Username, user.FullName, "token", now)
+	if err != nil {
+		errResponse := fmt.Errorf("failed to generate token: %v", err)
+		fmt.Println(errResponse)
+		return response.SendFailureResponse(ctx, fiber.StatusInternalServerError, "internal server error", nil)
+	}
+
+	refreshToken, err := jwt_token.GenerateToken(ctx.Context(), user.Username, user.FullName, "refresh_token", now)
+	if err != nil {
+		errResponse := fmt.Errorf("failed to generate refresh token: %v", err)
+		fmt.Println(errResponse)
+		return response.SendFailureResponse(ctx, fiber.StatusInternalServerError, "internal server error", nil)
+	}
+
+	resp.Username = user.Username
+	resp.FullName = user.FullName
+	resp.Token = token
+	resp.RefreshToken = refreshToken
+
+	return response.SendSuccessResponse(ctx, user)
 }
--- app/repository/user_repository.go
+++ app/repository/user_repository.go
@@ -9,3 +9,12 @@ import (
 func InsertNewUser(ctx context.Context, user *models.User) error {
 	return database.DB.Create(user).Error
 }
+
+func GetUserByUsername(ctx context.Context, username string) (models.User, error) {
+	var (
+		resp models.User
+		err  error
+	)
+	err = database.DB.Where("username = ?", username).Last(&resp).Error
+	return resp, err
+}
